2f54655e465cc63fedda9be559925eb4d976989c
Secondary Coding Computer <robotics@agnesirwin.org>
2023-01-28 11:32:53 -0500

    hey poopy

--- src/main/java/frc/robot/commands/AutoTimedDrive.java
+++ src/main/java/frc/robot/commands/AutoTimedDrive.java
@@ -6,14 +6,14 @@ import edu.wpi.first.wpilibj2.command.CommandBase;
 import frc.robot.subsystems.DriveSub;
 
 
-public class AutoDrive extends CommandBase {
+public class AutoTimedDrive extends CommandBase {
 	double outputMagnitude;
 	double curve;
 	double time;
 	Timer localTimeMain = new Timer();
 
 	
-    public AutoDrive(double time, double outputMagnitude, double curve) {
+    public AutoTimedDrive(double time, double outputMagnitude, double curve) {
      
     	this.outputMagnitude = outputMagnitude;
     	this.curve = curve;
